bb52aa3808fc26f77c3df5c7ed757478f644d2d8
Noah Duchan <nduchan@umich.edu>
2015-01-17 13:39:34 -0500

    Second commit

--- src/app.py
+++ src/app.py
@@ -1,11 +1,25 @@
 from flask import Flask
 from flask import render_template, request, redirect
 app = Flask(__name__)
-email_addresses = []
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/login', methods = ['POST'])
+def login():
+    login_email = request.form['EMAIL']
+    login_password = request.form['PASS']
+    print("login = " + login_email)
+    print("pass  = " + login_password)
+    if login_password == "password":
+        return redirect('/home')
+    else:
+        return redirect('/')
+    
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
